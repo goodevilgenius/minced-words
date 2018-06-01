07894e77d7dd9daffd723c0fa49449ece2a73e7e
Dan Jones <djones109+c9@gmail.com>
2018-06-01 17:08:57 +0000

    Parse site config file

--- scripts/build.js
+++ scripts/build.js
@@ -6,13 +6,19 @@ var Promise = require('bluebird');
 const hbs = require('handlebars');
 const fs = require('fs');
 const path = require('path');
+const yaml = Promise.promisifyAll(require('node-yaml'));
 
 const readFile = Promise.promisify(fs.readFile);
 const writeFile = Promise.promisify(fs.writeFile);
 
 let mainTemplate = function () {};
+let site = {};
 
-readFile(path.join(process.cwd(), 'templates', 'main.hbs'), 'utf8')
+yaml.readAsync(path.join(process.cwd(), '_config.yml'))
+    .then(function (siteConfig) {
+        site = siteConfig;
+        return readFile(path.join(process.cwd(), 'templates', 'main.hbs'), 'utf8');
+    })
     .catch(function (err) {
         console.error('Failed to read master template');
         console.error(err.message);
@@ -26,6 +32,6 @@ readFile(path.join(process.cwd(), 'templates', 'main.hbs'), 'utf8')
     .then(function (main) {
         return writeFile(
             path.join('build', 'index.html'),
-            main({content: '<h1>Hi</h1>'})
+            main({site: site, content: '<h1>Hi</h1>'})
         );
     });
