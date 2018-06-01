7a0a2be9c525cbfec24e355f98046cb585fe94b6
Dan Jones <djones109+c9@gmail.com>
2018-06-01 17:36:59 +0000

    Parse posts

--- scripts/build.js
+++ scripts/build.js
@@ -10,6 +10,7 @@ const yaml = Promise.promisifyAll(require('node-yaml'));
 
 let mainTemplate = function () {};
 let site = {};
+let posts = [];
 
 // Read config
 yaml.readAsync(path.join(process.cwd(), 'config.yml'))
@@ -23,13 +24,30 @@ yaml.readAsync(path.join(process.cwd(), 'config.yml'))
         // Compile main template
         mainTemplate = hbs.compile(template);
 
+        // Read posts
+        return fs.readdirAsync(path.join(process.cwd(), 'posts'));
+    })
+    .then(function (files) {
+        // Parse posts
+        return Promise.map(files, file => yaml.readAsync(path.join(
+            process.cwd(),
+            'posts',
+            file
+        )));
+    })
+    .then(function (parsedPosts) {
+        posts = parsedPosts;
+
         return Promise.resolve(true);
     })
     .then(function () {
         // Write index
         return fs.writeFileAsync(
             path.join('build', 'index.html'),
-            mainTemplate({site: site, content: '<h1>Hi</h1>'})
+            mainTemplate({
+                site: site,
+                content: '<h1>Hi</h1>\n' + JSON.stringify(posts)
+            })
         );
     })
     .catch(function (err) {
