142e231bad9ce87e5a234ebe28d993868234b7cb
Dan Jones <djones109+c9@gmail.com>
2018-06-05 18:32:04 +0000

    Start out archive.org import

---
+++ scripts/post_from_archive.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+
+'use strict';
+
+// The url to the _files.xml needs to be passed to this script
+
+var Promise = require('bluebird');
+const rp = require('request-promise');
+const parseString = Promise.promisify(require('xml2js').parseString);
+const path = require('path');
+
+const url = process.argv[2];
+const source = path.basename(url);
+const dir = path.dirname(url);
+
+let files = [];
+let meta = {};
+
+function mapXml (item) {
+    for (let k in item) {
+        if (k === '$') continue;
+        if (Array.isArray(item[k]) && item[k].length === 1) item[k] = item[k][0];
+    }
+    for (let k in item['$']) item[k] = item['$'][k];
+    delete item['$'];
+
+    return item;
+}
+
+rp(url)
+    .then(function (data) {
+        return parseString(data);
+    })
+    .then(function (filesObject) {
+        files = filesObject.files.file.map(mapXml);
+        
+        let meta = files.filter(file => 
+            file.name !== source && 
+            file.source === 'original' &&
+            file.format === 'Metadata' &&
+            file.name.endsWith('.xml')
+        ).shift();
+
+        console.log(files);
+        
+        console.log(meta);
+        
+        return rp(`${dir}/${meta.name}`);
+    }).then(function (metaString) {
+        return parseString(metaString);
+    }).then(function (metaObject) {
+        meta = mapXml(metaObject.metadata);
+        
+        console.log(meta);
+    });
