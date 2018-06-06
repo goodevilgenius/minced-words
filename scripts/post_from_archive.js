85b4f9b14f779425a91dabbd40123c31f8a97c50
Dan Jones <djones109+c9@gmail.com>
2018-06-06 17:22:12 +0000

    Write post from archive

--- scripts/post_from_archive.js
+++ scripts/post_from_archive.js
@@ -8,6 +8,7 @@ var Promise = require('bluebird');
 const rp = require('request-promise');
 const parseString = Promise.promisify(require('xml2js').parseString);
 const path = require('path');
+const yaml = Promise.promisifyAll(require('node-yaml'));
 
 const url = process.argv[2];
 const source = path.basename(url);
@@ -50,6 +51,40 @@ rp(url)
         return parseString(metaString);
     }).then(function (metaObject) {
         meta = mapXml(metaObject.metadata);
-        
         console.log(meta);
+
+        let dateStr = meta.publicdate || meta.addeddate;
+        if (!dateStr.endsWith('Z')) dateStr = `${dateStr}Z`;
+
+        let date = new Date(dateStr);
+
+        const title = meta.title.trim();
+        let slug = title
+            .replace(/ +/g, '-')
+            .replace(/[^A-Za-z0-9-]/g, '')
+            .replace(/-+$/, '').replace(/^-+/, '')
+            .toLowerCase();
+        let filename = `${date.toISOString().replace(/T.+/, '')}-${slug}`;
+
+        let post = {
+            archive_id: meta.identifier,
+            author: {
+                name: meta.creator,
+                email: meta.uploader
+            },
+            date: date.toISOString(),
+            title: title,
+            description: meta.description.replace(/<br *\/?>$/, ''),
+            language: meta.language,
+            enclosures: []
+        };
+
+        let filepath = path.join(process.cwd(), 'posts', filename);
+
+        return yaml.writeAsync(filepath, post);
+    })
+    .catch(function (err) {
+        console.error('Failed to build post');
+        console.error(err.message);
+        process.exit(1);
     });
